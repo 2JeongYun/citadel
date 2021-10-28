9638cfd132a347edf79bc642c6ade6dd9f80dd1e
2JeongYun <leejeongyun828@gmail.com>
2021-10-28 19:22:54 +0900

    Init

---
+++ src/test/java/com/neukrang/citadel/CitadelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neukrang.citadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
