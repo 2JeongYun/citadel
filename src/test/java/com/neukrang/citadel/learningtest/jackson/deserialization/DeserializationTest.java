024f4d41f5a02bf7070cd7c1dcefea9c16bb762b
2JeongYun <leejeongyun828@gmail.com>
2021-11-03 21:58:00 +0900

    테스트 추가: Jackson 역직렬화 러닝테스트 추가

---
+++ src/test/java/com/neukrang/citadel/learningtest/jackson/deserialization/DeserializationTest.java
@@ -0,0 +1,33 @@
+package com.neukrang.citadel.learningtest.jackson.deserialization;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neukrang.citadel.learningtest.jackson.testutil.TestUtil;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DeserializationTest {
+
+    String path = "./src/test/java/com/neukrang/citadel/learningtest/jackson/deserialization/";
+    ObjectMapper om = new ObjectMapper();
+
+    @Test
+    public void 역직렬화_필드명_매핑_JsonAlias() throws JsonProcessingException {
+        String json = TestUtil.fileToString(path + "testjson.txt");
+
+        TestObjectWithAlias testObjectWithAlias = om.readValue(json, TestObjectWithAlias.class);
+
+        Assertions.assertThat(testObjectWithAlias.getField1()).isEqualTo("필드1");
+        Assertions.assertThat(testObjectWithAlias.getField2()).isEqualTo("필드2");
+    }
+
+    @Test
+    public void 역직렬화_필드명_매핑_JsonSetter() throws JsonProcessingException {
+        String json = TestUtil.fileToString(path + "testjson.txt");
+
+        TestObjectWithSetter testObjectWithJsonSetter = om.readValue(json, TestObjectWithSetter.class);
+
+        Assertions.assertThat(testObjectWithJsonSetter.getField1()).isEqualTo("필드1");
+        Assertions.assertThat(testObjectWithJsonSetter.getField2()).isEqualTo("필드2");
+    }
+}
