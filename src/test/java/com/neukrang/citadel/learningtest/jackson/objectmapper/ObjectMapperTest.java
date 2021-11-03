9f43cac0f27e28ad5de3a7b8044d86640bbb2ba5
2JeongYun <leejeongyun828@gmail.com>
2021-11-03 21:41:41 +0900

    테스트 추가: TestUtil 추가

--- src/test/java/com/neukrang/citadel/learningtest/jackson/objectmapper/ObjectMapperTest.java
+++ src/test/java/com/neukrang/citadel/learningtest/jackson/objectmapper/ObjectMapperTest.java
@@ -4,19 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+
+import static com.neukrang.citadel.learningtest.jackson.testutil.TestUtil.fileToString;
 
 public class ObjectMapperTest {
 
-    String path = "./src/test/java/com/neukrang/citadel/learningtest/jackson/objectmapper";
+    String path = "./src/test/java/com/neukrang/citadel/learningtest/jackson/objectmapper/";
     ObjectMapper om = new ObjectMapper();
 
     @Test
-    public void 파일_읽기() throws IOException {
-        String result = fileToString("testjson.txt");
+    public void 파일_읽기() {
+        String result = fileToString(path + "testjson.txt");
         Assertions.assertThat(result).contains("{\n" +
                 "\"simpleName\": \"name1\",\n" +
                 "\"comName\": \"name2\"\n" +
@@ -25,21 +24,9 @@ public class ObjectMapperTest {
 
     @Test
     public void 심플_객체_역직렬화() throws IOException {
-        String fileStr = fileToString("testjson.txt");
+        String fileStr = fileToString(path + "testjson.txt");
 
         Simple simple = om.readValue(fileStr, Simple.class);
         Assertions.assertThat(simple.getSimpleName()).isEqualTo("name1");
-    }
-
-    public String fileToString(String fileName) throws IOException {
-        File f = new File(path + "/" + fileName);
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        StringBuffer sb = new StringBuffer();
-
-        String line = null;
-        while ((line = br.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        return sb.toString();
     }
 }
