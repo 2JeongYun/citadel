968bd7ac8edbdc1a012f9c0f31ab7383db17c5e7
2JeongYun <leejeongyun828@gmail.com>
2021-11-03 20:14:56 +0900

    테스트 변경: url 검증값 변경

--- src/test/java/com/neukrang/citadel/learningtest/etc/UrlEncoding.java
+++ src/test/java/com/neukrang/citadel/learningtest/etc/UrlEncoding.java
@@ -3,16 +3,16 @@ package com.neukrang.citadel.learningtest.etc;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class UrlEncoding {
 
     @Test
-    public void Url_인코딩() throws UnsupportedEncodingException {
+    public void Url_인코딩() {
         String target = "고급 참치캔";
-        String result = URLEncoder.encode(target, "UTF-8");
+        String result = URLEncoder.encode(target, StandardCharsets.UTF_8);
 
-        Assertions.assertThat(result).isEqualTo("%EA%B3%A0%EA%B8%89%20%EC%B0%B8%EC%B9%98%EC%BA%94");
+        Assertions.assertThat(result).isEqualTo("%EA%B3%A0%EA%B8%89+%EC%B0%B8%EC%B9%98%EC%BA%94");
     }
 }
