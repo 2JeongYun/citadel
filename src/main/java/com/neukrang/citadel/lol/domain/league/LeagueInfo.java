9740561953fb1c53015b8e22835b74db192f7567
2JeongYun <leejeongyun828@gmail.com>
2021-11-18 23:41:09 +0900

    변경: LeagueInfo, Summoner 에 JPA 적용, Entity 로 변경

--- src/main/java/com/neukrang/citadel/lol/domain/league/LeagueInfo.java
+++ src/main/java/com/neukrang/citadel/lol/domain/league/LeagueInfo.java
@@ -1,19 +1,39 @@
-package com.neukrang.citadel.lol.domain;
+package com.neukrang.citadel.lol.domain.league;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.neukrang.citadel.lol.domain.QueueType;
+import com.neukrang.citadel.lol.domain.Rank;
+import com.neukrang.citadel.lol.domain.Tier;
+import com.neukrang.citadel.lol.domain.summoner.Summoner;
 import lombok.Getter;
+
+import javax.persistence.*;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
 public class LeagueInfo {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "league_info_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
     private QueueType queueType;
+
+    @Enumerated(EnumType.STRING)
     private Tier tier;
+
     private int rank;
     private int leaguePoints;
     private int wins;
     private int losses;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "puuid")
+    private Summoner summoner;
 
     @JsonSetter("queueType")
     public void setQueueTypeWithJson(String queueTypeStr) {
@@ -23,5 +43,9 @@ public class LeagueInfo {
     @JsonSetter("rank")
     public void setTierWithJson(String rankStr) {
         this.rank = Rank.valueOf(rankStr).getNumber();
+    }
+
+    public void setSummoner(Summoner summoner) {
+        this.summoner = summoner;
     }
 }
