b8533574ffbc45cd24a66768f0617cf4bb6b8df2
2JeongYun <leejeongyun828@gmail.com>
2021-11-21 18:15:12 +0900

    변경: ApiKey 를 5분간 캐시하도록 변경

--- src/main/java/com/neukrang/citadel/config/security/apikey/ApiKeyAuthManager.java
+++ src/main/java/com/neukrang/citadel/config/security/apikey/ApiKeyAuthManager.java
@@ -1,35 +1,49 @@
 package com.neukrang.citadel.config.security.apikey;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class ApiKeyAuthManager implements AuthenticationManager {
 
     private final ApiKeyService apiKeyService;
-    private final Set<String> keySet = ConcurrentHashMap.newKeySet();
+    private final LoadingCache<String, ApiKey> apiKeyCache;
 
     public ApiKeyAuthManager(ApiKeyService apiKeyService) {
         this.apiKeyService = apiKeyService;
+        apiKeyCache = Caffeine.newBuilder()
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .build(new ApiKeyCacheLoader(apiKeyService));
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String key = (String) authentication.getPrincipal();
 
-        if (!keySet.contains(key)) {
-            Optional<ApiKey> apiKey = apiKeyService.findApiKeyByKey(key);
-            if (!apiKey.isPresent())
-                throw new BadCredentialsException("유효하지 않은 API KEY 입니다.");
+        if (apiKeyCache.get(key) == null)
+            throw new BadCredentialsException("유효하지 않은 API KEY 입니다.");
 
-            keySet.add(apiKey.get().getKey());
-        }
         authentication.setAuthenticated(true);
         return authentication;
+    }
+
+    @RequiredArgsConstructor
+    private static class ApiKeyCacheLoader implements CacheLoader<String, ApiKey> {
+
+        private final ApiKeyService apiKeyService;
+
+        @Override
+        public @Nullable ApiKey load(@NonNull String key) throws Exception {
+            return apiKeyService.findApiKeyByKey(key).orElse(null);
+        }
     }
 }
